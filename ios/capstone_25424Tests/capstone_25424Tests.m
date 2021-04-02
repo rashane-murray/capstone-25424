ebfb61ba957e3f4cdef0e1ed76d6115121363d30
Crowdbotics <team@crowdbotics.com>
2021-04-02 01:40:00 +0000

    Initial commit.

---
+++ ios/capstone_25424Tests/capstone_25424Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface capstone_25424 : XCTestCase
+
+@end
+
+@implementation capstone_25424
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://capstone-25424.botics.co",
+  defaultTimeout: 5000
+};
